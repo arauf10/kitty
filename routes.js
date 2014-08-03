6889a45a5af57de6561ca5144e454ae59b48863f
SpeedoDevo <speedo@devo.hu>
2014-08-03 04:44:29 +0100

    gravitz

--- routes.js
+++ routes.js
@@ -2,9 +2,14 @@ reqt = require('request');
 
 module.exports = function(app) {
 
-	app.get('/image/rand.jpg', function(req,res){
+	function random (low, high) {
+	    return Math.random() * (high - low) + low;
+	}
+
+
+	app.get('/image/:rand/rand.jpg', function(req,res){
 		reqt({
-			url: 'http://placekitty.artisan.io/200/200',
+			url: "http://placekitty.artisan.io/" + random(90, 110) + "/" + random(90,110),
 			encoding: null // Prevents Request from converting response to string
 		}, function(err, response, body) {
 			return res.end(body);
